592f55af81fc1ae271914b3fabd079ebc50b13fb
Andrew Arnott <andrewarnott@gmail.com>
2017-04-02 12:26:34 -0700

    Add binding redirect test, which fails on full MSBuild

---
+++ src/tests/ImmutableCollectionsConsumer1/ImmutableCollectionsConsumer1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace ImmutableCollectionsConsumers
+{
+    public static class ImmutableCollectionsConsumer1
+    {
+        public static ImmutableArray<string> GetArray() => ImmutableArray<string>.Empty;
+    }
+}
